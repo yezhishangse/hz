7541440b0079d9135c368087b08972ec4c12cbb2
yezhishangse <2731374523@qq.com>
2020-04-22 17:31:56 +0800

    init....

---
+++ hz-item/hz-item-service/src/main/java/com/hzit/item/HZService.java
@@ -0,0 +1,13 @@
+package com.hzit.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class HZService {
+    public static void main(String[] args) {
+        SpringApplication.run(HZService.class,args);
+    }
+}
